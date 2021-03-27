25135933101e9d020f44795bfc20570a31570d6e
Squido <squidocode@hotmail.com>
2021-03-27 18:54:12 +0100

    APIDOC finished

--- src/main/java/com/guillermo/DeadByDaylightAPI/config/OpenApiConf.java
+++ src/main/java/com/guillermo/DeadByDaylightAPI/config/OpenApiConf.java
@@ -7,8 +7,9 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
-public class OpenApiConf {
+public class OpenApiConf{
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
